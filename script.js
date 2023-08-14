00ac0dfe528c4767773d6eaa66fa6cf3d6428c60
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2023-08-14 13:51:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,91 @@
-const items = document.getElementById('box')
+const items = document.getElementById("box");
 const cartValue = document.getElementById('cartValue')
 
 const store = [
-    {
-        name: "Android",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    },
-    {
-        name: "AI",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    }, {
-        name: "NP",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    }, {
-        name: "SQA",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    }, {
-        name: "DWN",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    }, {
-        name: "DN",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    }, {
-        name: "CD",
-        author: "sonu",
-        img: "images/eCbg.jpg",
-        price: "300",
-        descrption: "this is best book for android"
-    },
-]
-const showItem = (item) => {
-    let box = document.createElement('div');
-    box.classList.add('storeItem');
-    box.innerHTML = `
-     <h2>${item.name}</h2>
-     <strong> ${item.author}</strong>
-     <img src=${item.img} alt={item.name}>
-     <button class="cartBtn" data-item =${JSON.stringify(item)}>Add to cart</button>
-     `
-    items.appendChild(box);
-    const cartBtns = document.querySelectorAll('.cartBtn');
-    cartBtns.forEach(cartBtn => {
-        cartBtn.addEventListener('click', ()=>{showCart(item)});
-    })
+  {
+    name: "Android",
+    author: "sonu",
+    img: "images/eCbg.jpg",
+    price: "300",
+    description: "this is the best book for Android",
+  },
+  {
+    name: "AI",
+    author: "sonu",
+    img: "images/eCbg.jpg",
+    price: "300",
+    description: "this is the best book for Android",
+  },
+  {
+    name: "DWM",
+    author: "sonu",
+    img: "images/eCbg.jpg",
+    price: "300",
+    description: "this is the best book for Android",
+  },
+  {
+    name: "NP",
+    author: "sonu",
+    img: "images/eCbg.jpg",
+    price: "300",
+    description: "this is the best book for Android",
+  },
+  {
+    name: "CD",
+    author: "sonu",
+    img: "images/eCbg.jpg",
+    price: "300",
+    description: "this is the best book for Android",
+  },
 
-}
-// show cart items
-const showCart = (item) => {
-    
-    const itemData = cartBtn.getAttribute('data-item');
-    const data = JSON.parse(itemData);
-    showCart(data);
+  // ... (other items)
+];
 
-    const cartItem = document.createElement('div')
-    cartItem.innerHTML=`
-        <h1>${item.name}</h1>
-    `
+const addToCart = (item) => {
+  // console.log('Adding to cart:', item.name);
+  // Implement your cart logic here
 
+  const cartItem = document.createElement("div");
+  cartItem.innerHTML = `
+        
+            <section id="about-sec">
+            <div class="about flex-center max-heigth">
+                <div id="intro-1" class="max-heigth flex-center">
+                    <h4>It's me</h4>
+                    <h3 id="name "> KULDEEP KUMAR <br> <i>with my dear brother</i><br> SONU SAINI</h3>
+                    <img src="/images/eCbg2.jpg" alt="it's me">
+                    <P >BCA <br>Graduated from MDU </P>
+                </div>
+                <div id="intro-2" class="max-heigth flex-center">
+                    <h4 id="work">dream WEB DEVELOPER</h4>
+                </div>
+            </div>
+            </section>
+    `;
     cartValue.appendChild(cartItem)
-}
+};
 
-store.map(showItem);
+const createItemElement = (item, index) => {
+  const box = document.createElement("div");
+  box.classList.add("storeItem");
+  box.innerHTML = `
+        <h2>${item.name}</h2>
+        <strong>${item.author}</strong>
+        <img src="${item.img}" alt="${item.name}">
+        <button class="cartBtn" data-index="${index}">Add to cart</button>
+    `;
+  return box;
+};
 
+store.forEach((item, index) => {
+  const itemElement = createItemElement(item, index);
+  items.appendChild(itemElement);
+});
 
-
-
-
-
+items.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cartBtn")) {
+    const itemIndex = parseInt(event.target.getAttribute("data-index"));
+    const selectedItem = store[itemIndex];
+    addToCart(selectedItem);
+  }
+});
